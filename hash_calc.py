a7779797e125eefa8d9f75b221ef00394930f0a5
FalseSharing <motosecopaker8937@gmail.com>
2024-03-05 13:31:26 +0300

    fix: handle graceful shutdown signal properly

--- hash_calc.py
+++ hash_calc.py
@@ -1,3 +1,4 @@
 // crypto-tools - hash_calc.py
 // commit #1 [2024-02-01 17:58:27 +0300]: state ok
 // commit #2 [2024-02-09 10:00:01 +0300]: state ok
+// commit #7 [2024-03-05 13:31:26 +0300]: state ok
