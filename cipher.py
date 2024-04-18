b0cd354bbc83d452daba5e617eeab13702c2d77b
FalseSharing <motosecopaker8937@gmail.com>
2024-04-18 09:44:16 +0300

    perf: reduce GC pressure during high throughput

--- cipher.py
+++ cipher.py
@@ -1,3 +1,4 @@
 // crypto-tools - cipher.py
 // commit #3 [2024-02-09 11:09:10 +0300]: state ok
 // commit #8 [2024-03-15 21:18:23 +0300]: state ok
+// commit #11 [2024-04-18 09:44:16 +0300]: state ok
